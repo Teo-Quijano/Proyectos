adc8e5bc69c9d55c1c08eb7b6b9f919f9029debb
Teo-Quijano <105201972+Teo-Quijano@users.noreply.github.com>
2022-05-13 22:01:39 +0100

    Actualizacion con nuevos retos completados

---
+++ Dia 3/dia3_reto4.js
@@ -0,0 +1,43 @@
+let miArray = 
+[
+
+{   nombre : "Emilio",
+    apellido : "Quijano",
+    anionacimiento : "2015",
+    aficiones : ["Jugar", "Patinar", "Leer", "Nadar", "Tablet"],
+    dni : {anioexpedicion : "2021", lugarnacimiento : "Lima"},
+    colorpelo : "Negro",
+}, 
+
+{   nombre : "Lucas",
+    apellido : "Quijano",
+    anionacimiento : "2011",
+    aficiones : ["Jugar", "Patinar", "Leer", "Nadar", "Nintendo"],
+    dni : {anioexpedicion : "2020", lugarnacimiento : "Lima"},
+    colorpelo : "Castano",
+},
+
+{   nombre : "Ami",
+    apellido : "Dulude",
+    anionacimiento : "1980",
+    aficiones : ["Bailar", "Playa", "Leer", "Comer", "Pintar"],
+    dni : {anioexpedicion : "2019", lugarnacimiento : "Lima"},
+    colorpelo : "Negro",
+}, 
+
+{   nombre : "Teo",
+    apellido : "Quijano",
+    anionacimiento : "1973",
+    aficiones : ["Netflix", "Dormir", "Leer", "Comer", "Cine"],
+    dni : {anioexpedicion : "2018", lugarnacimiento : "Caracas"},
+    colorpelo : "Negro",
+}]
+
+console.log(miArray);
+
+if (miArray.includes(colorpelo = "Castano")) {
+    print (nombre);
+}
+
+
+
---
+++ Dia 4/dia4_reto1.js
@@ -0,0 +1,13 @@
+for (let i = 0; i <10; i++) {
+    console.log(i+1);
+}
+
+for (let i = 2; i <11; i+= 2) {
+    console.log(i);
+}
+
+for (let i = 1; i <=5; i++) {
+    console.log(i*2-1);
+}
+
+
