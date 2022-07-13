0bb74b687a76096fcdaf476dfdd57f7efb38d57e
Teo-Quijano <105201972+Teo-Quijano@users.noreply.github.com>
2022-07-13 12:06:55 +0100

    Entrega Retos Dia 2

--- Dia 2/dia2_reto8.js
+++ Dia 2/dia2_reto8.js
@@ -10,8 +10,16 @@ let persona = {
       apellidosPadre: ["Quijano", "San Miguel"],
       edadPadre: 78,
       padresPadre: [
-        { nombreabupaterno: "Teodoro", apellidosabupaterno: ["Quijano", "Arbizu"], edadabupaterno: 90 },
-        { nombreabupaterna: "Julia", apellidosabupaterna: ["San Miguel", "Lopez"], edadabumaterna: 95 },
+        {
+          nombreabupaterno: "Teodoro",
+          apellidosabupaterno: ["Quijano", "Arbizu"],
+          edadabupaterno: 90,
+        },
+        {
+          nombreabupaterna: "Julia",
+          apellidosabupaterna: ["San Miguel", "Lopez"],
+          edadabupaterna: 95,
+        },
       ],
     },
 
@@ -20,10 +28,30 @@ let persona = {
       apellidosMadre: ["Fuenmayor", "Reyes"],
       edadMadre: 77,
       padresMadre: [
-        { nombreabumaterno: "Juan", apellidosabupaterno: ["Fuenmayor", "Matos"], edadabumaterno: 92 },
-        { nombreabumaterna: "Isabel", apellidosabumaterna: ["Reyes", "Tejera"], edadabumaterna: 88 },
+        {
+          nombreabumaterno: "Juan",
+          apellidosabumaterno: ["Fuenmayor", "Matos"],
+          edadabumaterno: 92,
+        },
+        {
+          nombreabumaterna: "Isabel",
+          apellidosabumaterna: ["Reyes", "Tejera"],
+          edadabumaterna: 88,
+        },
       ],
     },
   ],
 };
-        console.log(persona);
+console.log(persona);
+console.log(persona.padres[1].padresMadre[0].nombreabumaterno);
+console.log(persona.padres[0].padresPadre[1].apellidosabupaterna[1]);
+console.log(persona.padres[1].edadMadre + persona.padres[0].padresPadre[0].edadabupaterno);
+console.log((persona.padres[0].padresPadre[0].edadabupaterno = 95));
+console.log((persona.padres[0].padresPadre[1].edadabupaterna = 90));
+console.log(persona.padres[0].padresPadre[0].edadabupaterno);
+console.log(persona.padres[0].padresPadre[1].edadabupaterna);
+console.log(persona.padres[1].padresMadre[1].apellidosabumaterna.push(persona.padres[0].padresPadre[0].apellidosabupaterno[1]));
+console.log(persona.padres[1].padresMadre[1].apellidosabumaterna);
+console.log(persona.padres[1].apellidosMadre.shift());
+console.log(persona.padres[1].apellidosMadre);
+console.log(persona.padres[0].padresPadre[0].apellidosabupaterno[1].slice(-1));
